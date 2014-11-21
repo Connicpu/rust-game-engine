1a39fc230d6b4c8e4cf587307cff4e69a3f84682
Connorcpu <connorcpu@live.com>
2014-11-20 20:06:49 -0800

    Made a window show up!

--- src/engine_base/src/lib.rs
+++ src/engine_base/src/lib.rs
@@ -1,3 +1,43 @@
-pub fn it_works() {
-    println!("Engine called!")
+extern crate gl;
+extern crate glutin;
+
+pub fn engine_init(title: String, width: uint, height: uint) -> Game {
+    let game = Game {
+        title: title,
+        dimmensions: (width, height),
+    };
+    return game;
+}
+
+pub struct Game {
+    title: String,
+    dimmensions: (uint, uint),
+}
+
+impl Game {
+    pub fn run(self) {
+        let (game_width, game_height) = self.dimmensions;
+
+        let window = glutin::WindowBuilder::new()
+            .with_title(self.title)
+            .with_dimensions(game_width, game_height)
+            .with_vsync()
+            .build().unwrap();
+
+        unsafe { window.make_current() };
+
+        gl::load_with(|symbol| window.get_proc_address(symbol));
+
+        unsafe { gl::ClearColor(0.39, 0.58, 0.93, 1.0) };
+
+        while !window.is_closed() {
+            window.wait_events();
+
+            unsafe {
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+                gl::Flush();
+            };
+            window.swap_buffers();
+        }
+    }
 }
--- src/game/src/main.rs
+++ src/game/src/main.rs
@@ -1,6 +1,9 @@
 extern crate engine_base;
 
-fn main()
-{
-    engine_base::it_works()
+fn main() {
+    let game = engine_base::engine_init(
+        String::from_str("Test Game"),
+        1280, 720
+    );
+    game.run();
 }
